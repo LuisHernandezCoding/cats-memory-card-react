beb7742f5541b66e2f6bff27e789f700bfa2ac0c
LuisHernandezCoding <luishernandezcoding@gmail.com>
2023-01-26 13:22:10 -0600

    Created Footer Component

---
+++ src/components/layout/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Footer = () => (
+  <div>
+    <p>
+      Made with
+      <span className="heart"> ❤ </span>
+      by
+      {' '}
+      <a className="link" href="http://github.com/LuisHernandezCoding">Luis Hernandez</a>
+    </p>
+  </div>
+);
+
+export default Footer;
